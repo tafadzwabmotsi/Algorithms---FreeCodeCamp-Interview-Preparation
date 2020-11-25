bdb5a9963af805d09ed19c1e76f59d4781beedba
Tafadzwa Brian Motsi <70545914+tafadzwabmotsi@users.noreply.github.com>
2020-11-26 00:16:38 +0200

    Update bubbleSort.js

--- Algorithms/bubbleSort.js
+++ Algorithms/bubbleSort.js
@@ -15,7 +15,6 @@ function bubbleSort(array) {
     }
 
     return copyArray;
-    // Only change code above this line
   }
   
   bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
